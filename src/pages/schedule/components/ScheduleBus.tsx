346c10a531cb3301667bd19ee79ac13173cd632b
papakapa <kapustin.ks.00@gmail.com>
2021-01-15 18:00:10 +0300

    start update bus schedule page

--- src/pages/schedule/components/ScheduleBus.tsx
+++ src/pages/schedule/components/ScheduleBus.tsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {NavLink} from 'react-router-dom';
 
 const ScheduleBus: React.FC = () => {
+    const bus = [{
+        number:1,
+        route: 'Вишневец - Азот'
+    },{
+        number:2,
+        route: 'Девятовка - Победы'
+    },{
+        number:3,
+        route: 'Фолюш - Ожешко'
+    },{
+        number:4,
+        route: 'Барановичи - Пышки'
+    },{
+        number:5,
+        route: 'Вишневец - Азот'
+    },{
+        number:6,
+        route: 'Вишневец - Азот'
+    },{
+        number:7,
+        route: 'Вишневец - Азот'
+    },{
+        number:8,
+        route: 'Вишневец - Азот'
+    },{
+        number:9,
+        route: 'Вишневец - Азот'
+    },{
+        number:10,
+        route: 'Вишневец - Азот'
+    },{
+        number:11,
+        route: 'Вишневец - Азот'
+    }];
+
+    const rerenderBuses = useCallback(() => bus.map(el => <div><h1>{el.number}</h1><h2>{el.route}</h2></div>),[bus]);
+
     return (
         <div className='schedule__bus'>
             <ol className="navbar-nav">
@@ -12,7 +49,8 @@ const ScheduleBus: React.FC = () => {
                     <NavLink className="nav-link" to='/schedule/bus-number'>Bus Number</NavLink>
                 </li>
             </ol>
-            <h1>Bus Schedule</h1>
+            <h1>Your city is <span>Hrodna</span></h1>
+            {rerenderBuses()}
         </div>
 );
 };
